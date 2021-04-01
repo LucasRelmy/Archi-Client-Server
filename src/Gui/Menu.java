8efca072b7cf750ba5c3e6ade8aae2fa73fdf3d7
lucasrelmy <lucasrelmy@gmail.com>
2021-04-01 15:37:31 +0200

    Added GUI

---
+++ src/Gui/Menu.java
@@ -0,0 +1,12 @@
+package Gui;
+
+import javax.swing.*;
+
+public class Menu {
+    private JComboBox comboBox1;
+    private JTextField textField1;
+    private JList list1;
+    private JSpinner spinner1;
+    private JButton acheterButton;
+    private JButton afficherLesFacturesButton;
+}
